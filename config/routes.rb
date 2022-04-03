fbdbce798211d2c90248dad9f9ad1131f410b066
aknotj <100099666+aknotj@users.noreply.github.com>
2022-04-03 01:56:25 +0000

    [Finish] Meshiterro 18章

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/about" => "homes#about"
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+  end
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
